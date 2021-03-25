a2b42a9b0f9abef59c42e6c281fd730a11fee297
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-03-25 16:06:19 +0100

    Scritto un po' di codice ma c'è qualche problema

--- js/script.js
+++ js/script.js
@@ -4,5 +4,26 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // Chiedo all'utente di inserire una parola
+var parolaUtente = prompt('Inserisci una parola');
 
 // Creo la funzione
+
+// La funzione deve:
+// 
+// 
+// Analizzare la parola inserita dall'utente
+// Se la parola è uguale a sé stessa letta al contrario --> return -> la tua parola è un palindromo
+function trovaPalindromo (paroladaEsaminare) {
+    var parolaAlContrario = "";
+    for ( var i = paroladaEsaminare.length - 1; i > 0; i-- ) {
+        parolaAlContrario += paroladaEsaminare.charAt(i);
+    }
+
+    if (paroladaEsaminare == parolaAlContrario) {
+        alert("La parola inserita è un palindromo!");
+    } else {
+        alert("La parola inserita non è un palindromo!");
+    }
+}
+
+trovaPalindromo(parolaUtente);
