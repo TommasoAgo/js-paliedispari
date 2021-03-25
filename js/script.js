a2281554e532daa3074f85a3beef879d10fa88e2
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-03-25 15:20:11 +0100

    Creato repo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Palindroma
+
+// Chiedere all’utente di inserire una parola 
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Chiedo all'utente di inserire una parola
+
+// Creo la funzione
